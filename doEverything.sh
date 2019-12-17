e102d4d144ef21a170fc3dceec05bd347f5043f8
sonjeongwon621 <sonjeongwon621@gmail.com>
2019-12-17 17:06:23 -0500

    finalized version

--- doEverything.sh
+++ doEverything.sh
@@ -4,10 +4,16 @@ scriptDir="$(realpath $(dirname "$0"))" # get the path to this script itself
 
 sudo echo 'Authorizing sudo.'
 
-python ./Figures_Tables.py
+python ./Carroll_1997_RemARK.py #save figures and tables
 
-xelatex main.tex
+cd Paper
+xelatex main.tex #creates the main paper and aux file
+bibtex main.aux  #run bibtex to run aux to create bbl file
+xelatex main.tex #rerun latex with the bbl file
+
+cd ..
+cd Slides
 
 chmod u+x make_slides.sh #This is to grant permission to making the slides
 
-./make_slides.sh
+./make_slides.sh #create slides
