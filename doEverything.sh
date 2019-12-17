3ccc427094cf5614e6b2d929f0c00e7953700d4f
sonjeongwon621 <sonjeongwon621@gmail.com>
2019-12-16 21:06:23 -0500

    almost finished everything

--- doEverything.sh
+++ doEverything.sh
@@ -5,3 +5,9 @@ scriptDir="$(realpath $(dirname "$0"))" # get the path to this script itself
 sudo echo 'Authorizing sudo.'
 
 python ./Figures_Tables.py
+
+xelatex main.tex
+
+chmod u+x make_slides.sh #This is to grant permission to making the slides
+
+./make_slides.sh
